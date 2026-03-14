7d59d52389225d59ffba4ef27e5ea90ee57b36cb
Francis Lobo <francis@francis.com>
2026-03-14 19:35:18 +0100

    Fix: remove init_rc from Android.bp, use PRODUCT_COPY_FILES in device.mk

--- device/brcm/rpi5_custom/device.mk
+++ device/brcm/rpi5_custom/device.mk
@@ -8,3 +8,8 @@ PRODUCT_PACKAGES += \
     NameService \
     MySystemApp \
     MyUserApp
+
+# Install init rc files via PRODUCT_COPY_FILES to avoid Soong/Make bridge conflict
+PRODUCT_COPY_FILES += \
+    vendor/lobo_platform/vendor/lobo/services/fancontrol/fancontrol.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/fancontrol.rc \
+    vendor/lobo_platform/vendor/lobo/services/name_service/name_service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/name_service.rc
