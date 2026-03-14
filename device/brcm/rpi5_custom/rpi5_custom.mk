a2653ccbbaabaa750bbd5c2ba4c262a2132a6622
Francis Lobo <francis@francis.com>
2026-03-14 16:43:26 +0100

    Make rpi5_custom.mk self-contained, mirror upstream aosp_rpi5.mk pattern

--- device/brcm/rpi5_custom/rpi5_custom.mk
+++ device/brcm/rpi5_custom/rpi5_custom.mk
@@ -1,8 +1,16 @@
-# Inherit device packages
-$(call inherit-product, $(LOCAL_DIR)/device.mk)
+# Inherit everything from the upstream rpi5 product
+$(call inherit-product, device/brcm/rpi5/device.mk)
 
 PRODUCT_NAME         := rpi5_custom
 PRODUCT_DEVICE       := rpi5_custom
 PRODUCT_BRAND        := Lobo
 PRODUCT_MODEL        := Lobo RPi5
 PRODUCT_MANUFACTURER := Lobo
+
+# Lobo platform packages
+PRODUCT_PACKAGES += \
+    FanControlService \
+    FanSettingsService \
+    NameService \
+    MySystemApp \
+    MyUserApp
